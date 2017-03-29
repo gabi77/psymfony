2b4718e19baadefb3e6efe0ddb72c8bf587ab04e
CTO <CTO>
2017-03-29 09:28:33 +0200

    Testing Symfony

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new Gabi77\PlatformBundle\Gabi77PlatformBundle(),
+            new OC\PlatformBundle\OCPlatformBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
